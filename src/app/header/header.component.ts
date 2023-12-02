593f065e979ae1fa960d40a0cc35329943ae2826
Itai Barkav <itaibarkav@gmail.com>
2023-12-02 18:42:32 +0200

    style home and pharm

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 import { MaterialModule } from '../material.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, FooterComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
