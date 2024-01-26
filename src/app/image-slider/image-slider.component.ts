00fe08b29363b380252cf058e6434e06a9637df5
Itai Barkav <itaibarkav@gmail.com>
2024-01-27 01:49:45 +0200

    Fix sliders

--- src/app/image-slider/image-slider.component.ts
+++ src/app/image-slider/image-slider.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, Input } from '@angular/core';
 import { LottieModule } from 'ngx-lottie';
 import { Image } from './types';
 
@@ -9,31 +9,8 @@ import { Image } from './types';
   imports: [CommonModule, LottieModule],
   templateUrl: './image-slider.component.html',
   styleUrl: './image-slider.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ImageSliderComponent {
   @Input() images: Image[] = [];
-
-  currentIndex = 0;
-
-  next(): void {
-    if (this.currentIndex + 1 === this.images.length) {
-      this.currentIndex = 0;
-      return;
-    }
-
-    this.currentIndex++;
-  }
-
-  prev(): void {
-    if (this.currentIndex - 1 < 0) {
-      this.currentIndex = this.images.length - 1;
-      return;
-    }
-
-    this.currentIndex--;
-  }
-
-  goToStory(index: number): void {
-    this.currentIndex = index;
-  }
 }
--- src/app/pharm-yarok/personas/personas.component.ts
+++ src/app/pharm-yarok/personas/personas.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, Input } from '@angular/core';
 import { Persona } from './types';
 
 @Component({
@@ -8,31 +8,8 @@ import { Persona } from './types';
   imports: [CommonModule],
   templateUrl: './personas.component.html',
   styleUrl: './personas.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class PersonasComponent {
   @Input() personas: Persona[] = [];
-
-  currentIndex = 0;
-
-  next(): void {
-    if (this.currentIndex + 1 === this.personas.length) {
-      this.currentIndex = 0;
-      return;
-    }
-
-    this.currentIndex++;
-  }
-
-  prev(): void {
-    if (this.currentIndex - 1 < 0) {
-      this.currentIndex = this.personas.length - 1;
-      return;
-    }
-
-    this.currentIndex--;
-  }
-
-  goToStory(index: number): void {
-    this.currentIndex = index;
-  }
 }
