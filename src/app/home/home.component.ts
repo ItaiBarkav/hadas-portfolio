13ffc5344f90f4aa7dd9e029fac93b88c96f3db6
Itai Barkav <itaibarkav@gmail.com>
2024-02-05 00:14:20 +0200

    Fix about me in header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AbstractCaseComponent } from '../case/abstract-case/abstract-case.component';
 import { HeaderComponent } from '../header/header.component';
@@ -10,8 +11,20 @@ import { HeaderComponent } from '../header/header.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
+export class HomeComponent implements AfterViewInit {
+  private readonly ABOUT = 'about';
+  constructor(
+    private router: Router,
+    private viewportScroller: ViewportScroller
+  ) {}
+
+  ngAfterViewInit(): void {
+    if (this.router.url.includes(this.ABOUT)) {
+      setTimeout(() => {
+        this.viewportScroller.scrollToAnchor(this.ABOUT);
+      }, 2000);
+    }
+  }
 
   goto(path: string): void {
     this.router.navigateByUrl(`/${path}`);
